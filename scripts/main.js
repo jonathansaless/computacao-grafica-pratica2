437c349968ebfc058c17c39373441117a2bad665
jonathansaless <jonathan.sales@trt8.jus.br>
2023-06-28 12:30:24 -0300

    update

---
+++ scripts/main.js
@@ -0,0 +1,161 @@
+// Importar a biblioteca Three.js
+import * as THREE from "three";
+import { setController } from "./components/controller.js";
+import { createSky } from "./components/sky.js";
+import { createStreets } from "./components/street.js";
+import { createSquare } from "./components/public_square.js";
+import { GLTFLoader } from "GLTFLoader";
+import { Tween } from 'tween';
+
+let camera, scene, renderer, mesh, car1, car2, carMixer;
+init();
+
+
+function init() {
+    // SCENE
+    scene = new THREE.Scene();
+
+    // RENDERIZADOR
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x1F1F1F, 1);
+    // Renderizador ajusta a qualidade de renderização de acordo com a taxa de pixel do dispositivo
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild(renderer.domElement);
+
+    // CÂMERA
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 20000);
+    camera.position.set(0, 1000, 6500);
+
+    createCar();
+}
+
+
+setController(camera, renderer);
+createSky(scene, renderer);
+createSquare(scene);
+createStreets(scene);
+
+// const light = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+// scene.add(light);
+
+// loader = new GLTFLoader();
+// loader.load('/scripts/models/cartoon_car.glb', function(glb) {
+//     //glb.scale.set(50, 50, 50);
+//     scene.add(glb.scene)
+// });
+
+// ao mudar o tamanho da tela, será renderizado no novo tamanho da tela. Isso torna o projeto mais responsivo
+window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+
+// Criar a geometria do carro
+function createCar() {
+    const loader = new GLTFLoader();
+
+    loader.load('/scripts/models/cartoon_car.glb', function (glb) {
+        const car = glb.scene;
+        car.scale.set(35, 35, 35);
+        scene.add(car);
+
+        // Defina a posição inicial e final do modelo
+        const positions = [
+            new THREE.Vector3(4700, 0.1, -4600),
+            new THREE.Vector3(4700, 0.1, 4600),
+            new THREE.Vector3(-4700, 0.1, 4600),
+            new THREE.Vector3(-4700, 0.1, -4600),
+            new THREE.Vector3(4700, 0.1, -4600)
+        ];
+
+        // Defina a velocidade da animação
+        const speed = 0.002;
+
+        // Defina uma variável para rastrear o progresso da animação
+        let progress = 0;
+
+        // Defina o índice inicial do vetor de posições
+        let positionIndex = 0;
+
+        // Atualize a posição do modelo no loop de renderização
+        function update() {
+            requestAnimationFrame(update);
+
+            // Obtenha a posição inicial e final com base no índice atual
+            const initialPosition = positions[positionIndex];
+            const targetPosition = positions[positionIndex + 1];
+
+            // Interpole a posição do modelo entre a posição inicial e final com base no progresso
+            car.position.lerpVectors(initialPosition, targetPosition, progress);
+
+            // Atualize o progresso da animação
+            progress += speed;
+
+            // Verifique se a animação chegou ao fim
+            if (progress >= 1) {
+                progress = 0; // Reinicie o progresso para iniciar a próxima animação
+                positionIndex++; // Avance para a próxima posição no vetor
+                if (positionIndex >= positions.length - 1) {
+                    positionIndex = 0; // Reinicie o índice para começar novamente
+                }
+
+                // Calcule a direção do movimento
+                const direction = targetPosition.clone().sub(initialPosition).normalize();
+
+                // Calcule o ângulo de rotação
+                const angle = Math.atan2(-direction.z, direction.x);
+
+                // Aplique a rotação ao modelo
+                car.rotation.y = angle;
+            }
+
+            renderer.render(scene, camera);
+        }
+
+        // Inicie a animação
+        update();
+    });
+}
+
+
+
+
+
+// Função de animação
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+// Iniciar a animação
+animate();
+
+
+function carAnimation() {
+    if (695 <= swimmer.position.z && swimmer.position.z < 2000 && swimmer.position.x == 1200) {
+        if (swimmer.rotation.y == -300) {
+            swimmer.rotation.y = 0
+        }
+
+        swimmer.position.z += 5;
+
+    } else if (swimmer.position.x < -2000 && swimmer.position.z >= 700) {
+        swimmer.rotation.y = 600;
+        swimmer.position.z -= 5;
+
+    } else if (swimmer.position.z < 700) {
+        swimmer.rotation.y = -300;
+        swimmer.position.x += 5;
+
+    } else if (swimmer.position.z >= 2000) {
+        swimmer.rotation.y = 300;
+        swimmer.position.x -= 5;
+    }
+}
